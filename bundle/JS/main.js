336644bd651c7415a27ddb50e826b873915f93e5
Patryk Podolski <podolski.patryk@gmail.com>
2018-03-10 19:34:03 +0100

    interfaces | classes | inheritance

--- bundle/JS/main.js
+++ bundle/JS/main.js
@@ -1,9 +1,96 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 "use strict";
-console.log('second bundle works!!!');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+;
+var NewBiologyStudent = /** @class */ (function () {
+    function NewBiologyStudent() {
+        this.faculty = "Biology";
+    }
+    ;
+    return NewBiologyStudent;
+}());
+;
+var NewStudent = /** @class */ (function (_super) {
+    __extends(NewStudent, _super);
+    function NewStudent(name, lastName, age, yearOfStudies, gender, tongue, entryExamScores, finalGetInScore) {
+        var _this = _super.call(this) || this;
+        _this.name = name;
+        _this.lastName = lastName;
+        _this.age = age;
+        _this.yearOfStudies = yearOfStudies;
+        gender !== '' ? _this.gender = gender : '';
+        _this.tongue = tongue;
+        _this.entryExamScores = entryExamScores;
+        _this.finalGetInScore = _this.calculateGetInScore(entryExamScores);
+        return _this;
+    }
+    NewStudent.prototype.calculateGetInScore = function (scores) {
+        var finalScore;
+        finalScore = scores.reduce(function (a, b) {
+            return a + b;
+        });
+        return finalScore;
+    };
+    ;
+    return NewStudent;
+}(NewBiologyStudent));
+;
+var cW = new NewStudent("Caroline", "Wright", 20, 1, 'male', ['French', 'English'], [23, 55, 50, 44], 0);
+console.log(cW.name + ' scored ' + cW.finalGetInScore + ' and can speek ' + cW.tongue.reduce(function (a, b) { return a + ', ' + b; }) + '!');
+console.log(cW.name + ' studies ' + cW.faculty + '!');
+// *************** Accessors ***************************
+var passcode = "secret passcode";
+var Employee = /** @class */ (function () {
+    function Employee() {
+    }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                console.log("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee;
+}());
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+// ******************* STATIC PROPERTIES ************
+var driveDistance = /** @class */ (function () {
+    function driveDistance(priceForLiter) {
+        this.priceForLiter = priceForLiter;
+    }
+    driveDistance.prototype.getDistance = function (money) {
+        var fuelLiters = money.x / this.priceForLiter;
+        var km = (fuelLiters * 100) / driveDistance.fuelUsage["100km"];
+        return "For " + money.x + " PLN you can drive " + Math.round(km) + " kilometers...";
+    };
+    ;
+    driveDistance.fuelUsage = { '100km': 9 };
+    return driveDistance;
+}());
+var today = new driveDistance(3.16);
+var yesterday = new driveDistance(3.9);
+console.log(today.getDistance({ x: 100 }));
+console.log(yesterday.getDistance({ x: 100 }));
 
-},{}],2:[function(require,module,exports){
-"use strict";
-console.log('First bundle works!!!');
-
-},{}]},{},[1,2]);
+},{}]},{},[1]);
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,1 +1,206 @@
-console.log('First bundle works!!!');
+"use strict";
+
+// ********************* INTERFACE ***************
+
+interface StudentData{
+    name:string;
+    lastName: string;
+    age:number;
+    readonly yearOfStudies:number;
+    gender?:string;
+    tongue:Array<string>;
+    parents?:object;
+    [finalGetInScore: string]: any;
+};
+class NewBiologyStudent {
+    protected faculty: string; //members declared protected can also be accessed by instances of deriving classes; private can't
+
+    protected constructor(){ // new NewBiologyStudent can't be declared and accessed from outside a class byt can be extended ith super();
+        this.faculty = "Biology";
+    };
+};
+class NewStudent extends NewBiologyStudent implements StudentData{
+    readonly name:string;// property read only can only be assigned once when class instance is generated
+    readonly lastName: string;
+    age:number;
+    readonly yearOfStudies:number;
+    readonly gender?:string;
+    tongue:Array<string>;
+    readonly entryExamScores: Array<number>;
+    finalGetInScore:number;
+
+    constructor(name : string,lastName: string,age:number,yearOfStudies:number,gender:string,tongue:Array<string>,entryExamScores:Array<number>,finalGetInScore:number){
+        super();//must be called before this, inside constructor and in class that extends other class
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearOfStudies = yearOfStudies;
+        gender !== '' ? this.gender=gender:'';
+        this.tongue = tongue;
+        this.entryExamScores=entryExamScores;
+        this.finalGetInScore = this.calculateGetInScore(entryExamScores);
+        
+    }
+    
+    calculateGetInScore(scores:Array<number>): number{
+        let finalScore:number;
+
+        finalScore = scores.reduce(function(a,b): number{
+            return a+b;
+        });
+        return finalScore;
+    };
+};
+
+
+var cW: StudentData = new NewStudent("Caroline","Wright",20,1,'male',['French','English'],[23,55,50,44],0);
+
+
+console.log(cW.name + ' scored ' + cW.finalGetInScore + ' and can speek ' + cW.tongue.reduce(function(a,b):string{return a + ', ' + b}) + '!');
+
+console.log(cW.name + ' studies ' + cW.faculty + '!');
+
+// *************** Accessors ***************************
+
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName!: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+
+// ******************* STATIC PROPERTIES ************
+/**
+ * 
+ * 
+ * @class driveDistance
+ */
+class driveDistance {
+    static fuelUsage = {'100km':9};
+
+    getDistance(money:{x:number}){
+        let fuelLiters: number = money.x/this.priceForLiter;
+        let km:number = (fuelLiters * 100)/driveDistance.fuelUsage["100km"];
+        return "For " + money.x + " PLN you can drive " + Math.round(km) + " kilometers...";
+    }
+
+    constructor(public priceForLiter: number){};
+}   
+
+let today = new driveDistance(3.16);
+let yesterday = new driveDistance(3.9);
+
+console.log(today.getDistance({x:100}));
+console.log(yesterday.getDistance({x:100}));    
+
+
+// ************** ABSTRACT CLASS ****************
+
+abstract class Department {
+
+    constructor(public name: string) {
+    };
+
+    printName(): void {
+        console.log("Department name: " + this.name);
+    };
+
+    abstract printMeeting(): void; // must be implemented in derived classes
+};
+
+class AccountingDepartment extends Department {
+
+    constructor() {
+        super("Accounting and Auditing"); // constructors in derived classes must call super()
+    };
+
+    printMeeting(): void {
+        console.log("The Accounting Department meets each Monday at 10am.");
+    };
+
+    generateReports(): void {
+        console.log("Generating accounting reports...");
+    };
+};
+
+// let department: Department; // ok to create a reference to an abstract type
+// department = new Department(); // error: cannot create an instance of an abstract class
+// department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
+// department.printName();
+// department.printMeeting();
+// department.generateReports(); // error: method doesn't exist on declared abstract type
+
+// *********** Advanced Techniques **************
+
+//Using a class as an interface
+
+class Point {
+    x!: number;
+    y!: number;
+}
+
+interface Point3d extends Point {
+    z: number;
+}
+
+let point3d: Point3d = {x: 1, y: 2, z: 3};
+
+// ************ CLASS inheritance ***********
+
+class Car {
+    name!: string;
+    
+    constructor(n:string){
+        this.name = n;
+    };
+
+    maxSpeed(speed:number):void {
+        console.log(this.name + ' has maximum speed of' + speed);
+    };
+};
+
+class Renault extends Car {
+    constructor(n:string){
+        super(n);
+    };
+
+    maxSpeed(speed:number = 200){
+        console.log("French unreliable car...");
+        super.maxSpeed(speed);
+    };
+};
+
+class Hyundai extends Car {
+    constructor(n:string){
+        super(n);
+    };
+
+    maxSpeed(speed:number = 166){
+        console.log("Good japan car!");
+        super.maxSpeed(speed);
+    };
+};
+
+let twingo = new Renault("Twingo");
+let something = new Hyundai("Something");
+
+twingo.maxSpeed();
+something.maxSpeed(150);
