ffc88e720a4fe895d2b0fb186c23a8c98e6b45c6
Patryk Podolski <podolski.patryk@gmail.com>
2018-03-18 16:24:49 +0100

    finished with fliped chart excersise3

--- bundle/JS/rollDice.js
+++ bundle/JS/rollDice.js
@@ -5,16 +5,21 @@ var sqSizeDesc = sqSize + "px";
 var sqBorder = "15px solid black";
 var sqPadding = "10px";
 var sqMargin = "10px";
-var btn = document.createElement('button');
+var btn;
 var sqContainer = document.createElement('div');
 sqContainer.style.display = "inline-block";
 sqContainer.style.textAlign = 'centered';
-var sqSingle = document.createElement('div');
-sqSingle.style.border = sqBorder;
-sqSingle.style.padding = sqPadding;
-sqSingle.style.margin = sqMargin;
-sqSingle.style.width = sqSizeDesc;
-sqSingle.style.height = sqSizeDesc;
+// let sqSingle:HTMLElement = document.createElement('div');
+// sqSingle.style.border = sqBorder;
+// sqSingle.style.padding = sqPadding;
+// sqSingle.style.margin = sqMargin;
+// sqSingle.style.width = sqSizeDesc;
+// sqSingle.style.height = sqSizeDesc;
+/**
+ *
+ *
+ * @enum {number}
+ */
 var DiceNum;
 (function (DiceNum) {
     DiceNum[DiceNum["One"] = 0] = "One";
@@ -25,10 +30,20 @@ var DiceNum;
     DiceNum[DiceNum["Six"] = 5] = "Six";
 })(DiceNum || (DiceNum = {}));
 ;
-var sqColection = {
-    div: sqSingle
-};
+/**
+ *
+ *
+ * @class NewSq
+ */
 var NewSq = /** @class */ (function () {
+    /**
+     * Creates an instance of NewSq.
+     * @param {string} size
+     * @param {string} border
+     * @param {string} marg
+     * @param {string} padd
+     * @memberof NewSq
+     */
     function NewSq(size, border, marg, padd) {
         this.sq = document.createElement('div');
         this.sq.style.border = border;
@@ -37,20 +52,124 @@ var NewSq = /** @class */ (function () {
         this.sq.style.width = size;
         this.sq.style.height = size;
         this.sq.style.display = "inline-block";
+        this.sq.style.textAlign = "center";
+        this.createTextContainer();
     }
+    ;
+    /**
+     * @description function adds inner divs to position numbers right
+     *
+     * @memberof NewSq
+     */
+    NewSq.prototype.createTextContainer = function () {
+        var divTable = document.createElement('div');
+        var divToInsert = document.createElement('div');
+        //set up outer div
+        divTable.style.display = "table";
+        divTable.style.textAlign = "center";
+        divTable.style.width = "100%";
+        divTable.style.height = "100%";
+        //set up inner div
+        divToInsert.style.display = "table-cell";
+        divToInsert.style.verticalAlign = "middle";
+        divToInsert.textContent = "zero";
+        divToInsert.style.fontSize = "25px";
+        divToInsert.className = "squareToFlip";
+        divTable.appendChild(divToInsert);
+        this.sq.appendChild(divTable);
+    };
     ;
     return NewSq;
 }());
+;
+/**
+ * class creates button html element
+ * @namespace
+ * @property {HTMLElement} btn
+ *
+ * @class NewBtn
+ */
+var NewBtn = /** @class */ (function () {
+    /**
+     * Creates an instance of NewBtn.
+     * @memberof NewBtn
+     */
+    function NewBtn() {
+        this.btn = document.createElement("button");
+        this.btn.textContent = "FLIP";
+        this.btn.style.padding = "5px";
+        this.btn.style.fontSize = "30px";
+        this.btn.style.margin = "20px";
+        this.btn.style.display = "block";
+        this.btn.onclick = function () {
+            //flip dices function
+            console.log("works!");
+            flip();
+        };
+    }
+    ;
+    return NewBtn;
+}());
+;
+/**
+ * define new instance of
+ * @type {Array<NewSq>}
+ */
+var btn2 = new NewBtn;
+/**
+ * @description arrays being used to store html square elements
+ * @type {Array<NewSq>}
+ */
 var sqArray = [];
-for (var i = 0; i < 4; i++) {
-    sqArray.push(new NewSq(sqSizeDesc, sqBorder, sqMargin, sqPadding));
+/**
+ * function creating new HtmlElement with use of class NewSq
+ *
+ * @param {Array<NewSq>} arrToPass
+ * @returns {Array<NewSq>}
+ */
+function fillArrWithHTMLSquares(arrToPass) {
+    for (var i = 0; i < 4; i++) {
+        arrToPass.push(new NewSq(sqSizeDesc, sqBorder, sqMargin, sqPadding));
+    }
+    ;
+    return arrToPass;
 }
-for (var i = 0; i < 4; i++) {
-    console.log(typeof (sqArray[i].sq));
-    sqContainer.appendChild(sqArray[i].sq);
+;
+/**
+ * Function creates one html element out of array containig htmlElements
+ *
+ * @param {Array<NewSq>} arrToPass
+ * @param {HTMLElement} sqContainer
+ * @returns {HTMLElement}
+ */
+function buildHtmlOutOfHtmlElementsArray(arrToPass, sqContainer) {
+    for (var i = 0; i < 4; i++) {
+        // console.log(typeof(sqArray[i].sq));
+        sqContainer.appendChild(arrToPass[i].sq);
+    }
+    ;
+    return sqContainer;
 }
-sqContainer.appendChild(sqSingle);
-document.body.appendChild(btn);
+;
+/**
+ * creates new instance of a button
+ */
+function flip() {
+    var sqCol = document.getElementsByClassName("squareToFlip");
+    for (var i = 0; i < sqCol.length; i++) {
+        sqCol[i].innerText = DiceNum[getRandomNumber(0, 5)];
+    }
+    ;
+}
+;
+var getRandomNumber = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+sqArray = fillArrWithHTMLSquares(sqArray);
+sqContainer = buildHtmlOutOfHtmlElementsArray(sqArray, sqContainer);
+document.body.appendChild(btn2.btn);
 document.body.appendChild(sqContainer);
 
 },{}]},{},[1]);
--- typescript/diceRollApp.ts
+++ typescript/diceRollApp.ts
@@ -6,19 +6,23 @@ let sqBorder: string = "15px solid black";
 let sqPadding: string = "10px";
 let sqMargin: string = "10px";
 
-let btn: Element = document.createElement('button');
+let btn: HTMLElement;
 let sqContainer: Element = document.createElement('div');
 (sqContainer as HTMLElement).style.display = "inline-block";
 (sqContainer as HTMLElement).style.textAlign = 'centered';
 
-let sqSingle:HTMLElement = document.createElement('div');
-sqSingle.style.border = sqBorder;
-sqSingle.style.padding = sqPadding;
-sqSingle.style.margin = sqMargin;
-sqSingle.style.width = sqSizeDesc;
-sqSingle.style.height = sqSizeDesc;
+// let sqSingle:HTMLElement = document.createElement('div');
+// sqSingle.style.border = sqBorder;
+// sqSingle.style.padding = sqPadding;
+// sqSingle.style.margin = sqMargin;
+// sqSingle.style.width = sqSizeDesc;
+// sqSingle.style.height = sqSizeDesc;
 
-
+/**
+ * 
+ * 
+ * @enum {number}
+ */
 enum DiceNum {
     One,
     Two,
@@ -27,19 +31,30 @@ enum DiceNum {
     Five,
     Six
 };
-
+/**
+ * 
+ * 
+ * @interface sqEl
+ */
 interface sqEl {
-    div: HTMLElement;
-}
-
-let sqColection: sqEl = {
-    div:sqSingle
-}
-
-class NewSq {
     sq: HTMLElement;
+}
 
-
+/**
+ * 
+ * 
+ * @class NewSq
+ */
+class NewSq implements sqEl{
+    sq: HTMLElement;
+    /**
+     * Creates an instance of NewSq.
+     * @param {string} size 
+     * @param {string} border 
+     * @param {string} marg 
+     * @param {string} padd 
+     * @memberof NewSq
+     */
     constructor(size:string,border:string,marg:string,padd: string,){
         this.sq = document.createElement('div');
         this.sq.style.border = border;
@@ -48,27 +63,133 @@ class NewSq {
         this.sq.style.width = size;
         this.sq.style.height = size;
         this.sq.style.display = "inline-block";
+        this.sq.style.textAlign = "center";
+        this.createTextContainer();
+
+    };
+    /**
+     * @description function adds inner divs to position numbers right
+     * 
+     * @memberof NewSq
+     */
+    createTextContainer():void {
+        let divTable:HTMLElement = document.createElement('div');
+        let divToInsert: HTMLElement = document.createElement('div');
+        //set up outer div
+        divTable.style.display = "table";
+        divTable.style.textAlign = "center";
+        divTable.style.width = "100%";
+        divTable.style.height = "100%";
+        //set up inner div
+        divToInsert.style.display = "table-cell";
+        divToInsert.style.verticalAlign = "middle";
+        divToInsert.textContent = "zero";
+        divToInsert.style.fontSize = "25px";
+        divToInsert.className = "squareToFlip";
+
+        divTable.appendChild(divToInsert);
+
+        this.sq.appendChild(divTable);
     };
 
+};
+/**
+ * @property {HTMLElement} btn
+ * @interface btnEl
+ */
+interface btnEl {
+    btn:HTMLElement;
 }
 
+/**
+ * class creates button html element
+ * @namespace
+ * @property {HTMLElement} btn
+ * 
+ * @class NewBtn
+ */
+class NewBtn implements btnEl {
+    btn:HTMLElement;
+    /**
+     * Creates an instance of NewBtn.
+     * @memberof NewBtn
+     */
+    constructor(){
+        this.btn = document.createElement("button");
+        this.btn.textContent = "FLIP";
+        this.btn.style.padding = "5px";
+        this.btn.style.fontSize = "30px";
+        this.btn.style.margin = "20px";
+        this.btn.style.display = "block";
+        this.btn.onclick = function():void {
+            //flip dices function
+            console.log("works!");
+            flip();
+        };
+    };
 
+};
+/**
+ * define new instance of 
+ * @type {Array<NewSq>}
+ */
+let btn2:btnEl = new NewBtn;
+/**
+ * @description arrays being used to store html square elements
+ * @type {Array<NewSq>} 
+ */
 let sqArray: Array<NewSq> = [];
+/**
+ * function creating new HtmlElement with use of class NewSq
+ * 
+ * @param {Array<NewSq>} arrToPass 
+ * @returns {Array<NewSq>} 
+ */
+function fillArrWithHTMLSquares(arrToPass:Array<NewSq>):Array<NewSq> {
+    for (let i:number=0;i<4;i++){
+        arrToPass.push(new NewSq(sqSizeDesc, sqBorder, sqMargin, sqPadding));
+    };
 
-for (let i:number=0;i<4;i++){
-    sqArray.push(new NewSq(sqSizeDesc, sqBorder, sqMargin, sqPadding));
-}
+    return arrToPass;
+};
+/**
+ * Function creates one html element out of array containig htmlElements
+ * 
+ * @param {Array<NewSq>} arrToPass 
+ * @param {HTMLElement} sqContainer 
+ * @returns {HTMLElement} 
+ */
+function buildHtmlOutOfHtmlElementsArray(arrToPass:Array<NewSq>,sqContainer:HTMLElement):HTMLElement{
+    for (let i:number=0;i<4;i++){
+        // console.log(typeof(sqArray[i].sq));
+        sqContainer.appendChild(arrToPass[i].sq);
+    };
 
+    return sqContainer;
+};
 
-for (let i:number=0;i<4;i++){
-    console.log(typeof(sqArray[i].sq));
-    sqContainer.appendChild(sqArray[i].sq);
-}
+/**
+ * creates new instance of a button
+ */
 
+function flip():void {
+    let sqCol:HTMLCollectionOf<Element> = document.getElementsByClassName("squareToFlip");
+    
+    for (let i:number=0;i<sqCol.length;i++){
+        (sqCol[i] as HTMLElement).innerText = DiceNum[getRandomNumber(0,5)]; 
+    };
+};
 
-sqContainer.appendChild(sqSingle);
+let getRandomNumber: Function = function(min:number,max:number): number{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min+1))+min;
+};
 
-document.body.appendChild(btn);
+sqArray = fillArrWithHTMLSquares(sqArray);
+sqContainer = buildHtmlOutOfHtmlElementsArray(sqArray,(sqContainer as HTMLElement));
+
+document.body.appendChild(btn2.btn);
 document.body.appendChild(sqContainer);
 
 
